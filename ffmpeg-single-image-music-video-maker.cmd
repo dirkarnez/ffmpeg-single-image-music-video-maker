3cdd91275388d4ec8739d5482a66be5b17996718
Dirk Arnez <smalldirkalex@gmail.com>
2024-04-18 20:58:39 +0800

    - update ffmpeg-single-image-music-video-maker.cmd

--- ffmpeg-single-image-music-video-maker.cmd
+++ ffmpeg-single-image-music-video-maker.cmd
@@ -1,7 +1,5 @@
 @echo off
-set /p image="Drag an image file: "
-set /p music="Drag a music file: "
-set /p filename="Enter output file name / full path: "
+set PATH=%USERPROFILE%\Downloads\ffmpeg-6.0-full_build-shared\ffmpeg-6.0-full_build-shared\bin
 @REM ffmpeg -framerate 30 -s 1920x1080 -i %image% -i %music% -vcodec libx264 -crf 25  -pix_fmt yuv420p "%filename%"
-ffmpeg.exe -loop 1 -i vlcsnap.jpg -i output.wav -c:v libx264 -tune stillimage -c:a aac -pix_fmt yuv420p -shortest out.mp4
+ffmpeg.exe -loop 1 -i "xxxx.jpg" -i "xxxxx.mp3" -c:v libx264 -tune stillimage -c:a aac -pix_fmt yuvj420p -shortest out2.mp4
 pause
