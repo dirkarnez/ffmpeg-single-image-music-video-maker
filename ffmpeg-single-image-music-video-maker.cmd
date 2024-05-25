17d87fe9750167dd2ab4a4a192f5af816c032665
Dirk Arnez <smalldirkalex@gmail.com>
2024-05-25 22:00:51 +0800

    - update ffmpeg-single-image-music-video-maker.cmd

--- ffmpeg-single-image-music-video-maker.cmd
+++ ffmpeg-single-image-music-video-maker.cmd
@@ -1,5 +1,5 @@
 @echo off
 set PATH=%USERPROFILE%\Downloads\ffmpeg-6.0-full_build-shared\ffmpeg-6.0-full_build-shared\bin
-@REM ffmpeg -framerate 30 -s 1920x1080 -i %image% -i %music% -vcodec libx264 -crf 25  -pix_fmt yuv420p "%filename%"
-ffmpeg.exe -loop 1 -i "xxxx.jpg" -i "xxxxx.mp3" -c:v libx264 -tune stillimage -c:a aac -pix_fmt yuvj420p -shortest out2.mp4
+ffmpeg -framerate 30 -s 1920x1080 -i "xxxx.jpg" -i "xxxx.mp3" -vcodec libx264 -c:a aac -pix_fmt yuvj420p "xxxx.mp4"
+@REM ffmpeg.exe -loop 1 -i "xxxx.jpg" -i "xxxx.mp3" -c:v libx264 -tune stillimage -c:a aac -pix_fmt yuvj420p -shortest "xxxx.mp4"
 pause
